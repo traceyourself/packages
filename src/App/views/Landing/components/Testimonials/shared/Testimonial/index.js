78dbac8f9a1a110068aca361b7fb0f08ea48990a
ancestorcloud2 <james389205+git@gmail.com>
2016-12-01 16:46:25 -0700

    lots of stuff

--- src/App/views/Landing/components/Testimonials/shared/Testimonial/index.js
+++ src/App/views/Landing/components/Testimonials/shared/Testimonial/index.js
@@ -1,38 +1,72 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import { X, Y } from 'obj.Layout'
 
-import { StyleSheet, css } from 'aphrodite'
 import { colors } from 'App/style/settings'
 
-export default ({avatar, name, subname, quote}) =>
-  <X y>
+const TestimonialUI = ({
+  avatar, name, subname, quote,
+  verticalLayout
+}) => {
+  const nameNode =
     <div style={{
-      marginRight: '16px'
-    }}>
-      <img {...{ src: avatar, className: css(styles.avatar) }} />
-    </div>
-    <div>
-      <div style={{
-        color: colors.slate[2],
-        fontWeight: 'bold',
-        fontSize: '1.1em'
-      }}>{name}, {subname}</div>
-      <div {...{
-        style: {
-          marginTop: '8px',
-          borderRadius: '12px',
-          border: '2px solid #99B0C3',
-          backgroundColor: '#E5F0FA',
-          padding: '20px'
-        }
-      }}>{'"'}{quote}{'"'}</div>
-    </div>
-  </X>
+      color: colors.slate[2],
+      fontWeight: 'bold',
+      fontSize: '1.1em'
+    }}>{name}, {subname}</div>
 
-const styles = StyleSheet.create({
-  avatar: {
-    borderRadius: '50%',
-    width: '90px'
-  }
+  const quoteNode =
+    <div {...{
+      style: {
+        marginTop: '8px',
+        borderRadius: '12px',
+        border: '2px solid #99B0C3',
+        backgroundColor: '#E5F0FA',
+        padding: '20px'
+      }
+    }}>{'"'}{quote}{'"'}</div>
+
+  const avatarSize = verticalLayout ? '30px' : '90px'
+  const avatarNode =
+    <img {...{
+      src: avatar,
+      style: {
+        borderRadius: '50%',
+        width: avatarSize,
+        height: avatarSize
+      }
+    }} />
+
+  return verticalLayout
+    ? (
+      <Y>
+        <X x>
+          <div style={{marginRight: '8px'}}>
+            {avatarNode}
+          </div>
+          {nameNode}
+        </X>
+        {quoteNode}
+      </Y>
+    )
+    : (
+      <X y>
+        <div style={{
+          marginRight: '16px'
+        }}>
+          {avatarNode}
+        </div>
+        <div>
+          {nameNode}
+          {quoteNode}
+        </div>
+      </X>
+    )
+}
+
+const mapStateToProps = ({ viewState: { viewportSize } }) => ({
+  verticalLayout: viewportSize.width < 600
 })
+
+export default connect(mapStateToProps)(TestimonialUI)
